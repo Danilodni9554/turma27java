22328bdb8158bb67690f7dfa9cc50e1b5db73ddd
Danilodni9554 <danilopesan@gmail.com>
2021-07-23 18:42:07 -0300

    atualizacao 23 07

---
+++ mysql/Lista2Exerc1.sql
@@ -0,0 +1,5 @@
+insert into tb_classe(titulo, genero, lancamento) values ("God of War", "Kratos", '2005-03-22');
+insert into tb_classe(titulo, genero, lancamento) values ("Super Mario", "Mario", '2007-11-01');
+insert into tb_classe(titulo, genero, lancamento) values ("GTA V", "Trevor", '2013-09-17');
+insert into tb_classe(titulo, genero, lancamento) values ("Tony Hawks", "Tony", '2000-09-19');
+insert into tb_classe(titulo, genero, lancamento) values ("Zelda", "Link", '1998-11-21');
